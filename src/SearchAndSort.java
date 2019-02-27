4413b63f33ea0f834da4640ee1e450b756d065f4
ncoreus <ncoreus@ucvts.tec.nj.us>
2019-02-27 14:45:11 -0500

    updated

--- src/SearchAndSort.java
+++ src/SearchAndSort.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class SearchAndSort {
 	
 	/**
@@ -7,6 +8,36 @@ public class SearchAndSort {
 	 */
 	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("what algorithm would you like to execute?");
+		String algorithm = in.nextLine();
+		System.out.println();
+		System.out.print("what type of data?");
+		String data = in.nextLine();
+		System.out.println();
+		System.out.print("how is it stored? (array or list)");
+		String stored = in.nextLine();
+		System.out.println();
 		
+		
+		switch(algorithm.toLowerCase()) {
+			case "bubble":
+				break;
+			case "selection":
+				break;
+			case "insertion":
+				break;
+			case "merge":
+				break;
+			case "linear":
+				break;
+			case "binary":
+				break;
+			case "quit":
+				break;
+			default:
+				System.out.print("Invalid input");
+				break;
+		}
 	}
 }
